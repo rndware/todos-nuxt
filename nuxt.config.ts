cde2a428fc2c4d1b24c753fa7eabdb8851ddcbad
rndware <Adam.Smith@toshibagcs.com>
2024-07-18 12:43:17 +0100

    Refactor sass imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,12 @@ export default defineNuxtConfig({
     // ...
     "@pinia/nuxt",
   ],
-  css: ["primeicons/primeicons.css"],
+  css: ["primeicons/primeicons.css", '@/assets/scss/main.scss'],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@import "@/assets/scss/variables.scss";',
+          additionalData: '@import "@/assets/scss/_variables.scss";',
         },
       },
     },
